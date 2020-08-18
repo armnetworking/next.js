d223c38b5fc122aae137e777b0b152137bd6396f
armnetworking <36816666+armnetworking@users.noreply.github.com>
2020-08-18 16:53:35 +0300

    no header alert.js v2

--- components/alert.js
+++ components/alert.js
@@ -23,7 +23,7 @@ export default function Alert({ preview }) {
               </a>{' '}
               to exit preview mode.
             </>
-/*          ) : ( // Raz - no headline
+          ) : /*( // Raz - no headline
             <>
               The source code for this blog is{' '}
               <a
