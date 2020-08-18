206b8fb81445b040a43fe0dee4a73026bf7c4c0d
armnetworking <36816666+armnetworking@users.noreply.github.com>
2020-08-18 18:39:04 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
+//------ /components/alert.js ------
+export function mesaj {
+    "/Learn from the mistakes of others. You can’t live long enough to make them all yourself./"/ -/Eleanor Roosevelt
+}
+export function alerta {
+    "/Holding onto your anger is like drinking poison and expecting the other person to die./”/ -/Buddha
+}
+
 module.exports = {
   async headers() {
     return [
