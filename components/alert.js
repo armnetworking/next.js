54e6a646e33f7315321de47c1894d65b0104f6dc
armnetworking <36816666+armnetworking@users.noreply.github.com>
2020-08-18 16:55:31 +0300

    Update alert.js

--- components/alert.js
+++ components/alert.js
@@ -23,7 +23,7 @@ export default function Alert({ preview }) {
               </a>{' '}
               to exit preview mode.
             </>
-/*          ) : ( // Raz - no headline
+          ) : /*( // Raz - no headline
             <>
               The source code for this blog is{' '}
               <a
@@ -33,8 +33,8 @@ export default function Alert({ preview }) {
                 available on GitHub
               </a>
               .
-            </> */
-          )}
+            </> 
+          )*/}  // close no headline
         </div>
       </Container>
     </div>
